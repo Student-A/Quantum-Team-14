264a07605d9c20cd3a3664d51969327a60d21704
Student-A <ai.aleryani@jacobs-university.de>
2019-10-19 21:58:24 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
 import pygame, sys, math
 
+WIN_W = 800
+WIN_H = 600
 
 class Game:
-    def __init__(self):
+    def __init__(self, cellsize = [WIN_W/7, WIN_H/7]):
         self.FPS = 30
-        self.windowWidth = 800
-        self.windowHeight = 600
 
+        self.cellsize = cellsize
+        
         # number of "blinks" for proximity cue
         self.proximityBlinks = 3
         
         self.isProximityCue = False
     
         pygame.init()
-        self.display = pygame.display.set_mode((self.windowWidth, self.windowHeight))
+        self.display = pygame.display.set_mode((WIN_W, WIN_H))
         pygame.display.set_caption("Treasure Hunting in the Quantum Regime")
         self.clock = pygame.time.Clock()
 
-        self.character = {image: None, rect: None, position: (0, 0), type: "Classical"}
+        self.character = Character(r'./res/char.png', "Classical")
         
         self.loadResources()
-
     def loadResources(self):
         #self.cueFilter = pygame.image.load(r'./res/cuebg.png')
         #self.cueFilterRect = self.cueFilter.get_rect()
-        #self.cueFilter = pygame.transform.scale(self.cueFilter, (self.windowWidth, self.windowHeight))
+        #self.cueFilter = pygame.transform.scale(self.cueFilter, (WIN_W, WIN_H))
 
-        self.cueFilter = pygame.Surface((self.windowWidth,self.windowHeight), pygame.SRCALPHA)
+        self.cueFilter = pygame.Surface((WIN_W,WIN_H), pygame.SRCALPHA)
         
         self.groundBg = pygame.image.load(r'./res/bg.png')
         self.groundBgRect = self.groundBg.get_rect()
-        self.groundBg = pygame.transform.scale(self.groundBg, (self.windowWidth, self.windowHeight))
+        self.groundBg = pygame.transform.scale(self.groundBg, (WIN_W, WIN_H))
 
-        self.character.image = pygame.image.load(r'./res/char.png')
+        self.character.loadResources(WIN_W//10, WIN_W//10)
         
     # proximity: float between [0, 1]
     def triggerProximityCue(self, proximity):
@@ -47,17 +48,38 @@ class Game:
                 sys.exit()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                self.triggerProximityCue(0.0)
+                self.triggerProximityCue(1.0)
 
+            if event.type == pygame.KEYDOWN:
+                if not self.character.isMoving:
+                    if event.key == pygame.K_LEFT:
+                        self.character.triggerMovement("x", -self.cellsize[0], 5)
+                    if event.key == pygame.K_RIGHT:
+                        self.character.triggerMovement("x", self.cellsize[0], 5)
+                    if event.key == pygame.K_UP:
+                        self.character.triggerMovement("y", -self.cellsize[1], 5)
+                    if event.key == pygame.K_DOWN:
+                        self.character.triggerMovement("y", self.cellsize[1], 5)
+                        
     def renderGraphics(self):
+        # draw background
         self.display.blit(self.groundBg, (0, 0))
 
+        # draw character
+        self.display.blit(self.character.getImage(), self.character.position)
+        if self.character.position[0]+self.cellsize[0] > WIN_W:
+            self.display.blit(self.character.getImage(), ((self.character.position[0]+self.cellsize[0]) % WIN_W - self.cellsize[0], self.character.position[1]))
+
+        if (self.character.position[1]+self.cellsize[1]) > WIN_H:
+            self.display.blit(self.character.getImage(), (self.character.position[0], (self.character.position[1]+self.cellsize[1]) % WIN_H - self.cellsize[1]))
+
+        
         if self.isProximityCue:
             self.cueFilter.fill((*self.proximityCueColour, 255*(0.5 + 0.5*math.sin(self.currentProximityPhase))))
             self.display.blit(self.cueFilter, (0, 0))
 
             self.cueFilter.fill((*self.proximityCueColour, 255*(0.5 + 0.5*math.sin(self.currentProximityPhase))))
-
+            
             #pygame.draw.rect(self.display,
             #                 (*self.proximityCueColour, 0.5 + 0.5*math.sin(self.currentProximityPhase)),
             #                 self.display.get_rect())
@@ -66,9 +88,9 @@ class Game:
                 self.isProximityCue = False
             
     def run(self):
-        while True:
-            
+        while True:            
             self.handleEvents()
+            self.character.update()
             self.renderGraphics()
             
             pygame.display.update()
@@ -76,6 +98,56 @@ class Game:
             self.display.fill((0, 0, 0))
             self.clock.tick(self.FPS)
 
+class Character:
+    def __init__(self, imagedir, movementType, position = [0, 0]):
+        self.image = None
+        self.rect = None
+        self.orientation = "right"
+        self.position = position
+        self.imagedir = imagedir
+        self.movementType = movementType
+
+        self.isMoving = False
+        
+    def loadResources(self, w, h):
+        self.image = pygame.transform.scale(pygame.image.load(self.imagedir), (w, h))
+        self.images = {"left": pygame.transform.rotate(self.image, 180),
+                       "right": self.image,
+                       "up": pygame.transform.rotate(self.image, 90),
+                       "down": pygame.transform.rotate(self.image, 270)}
+        
+        self.rect = self.image.get_rect()
+
+    # axis is "x" or "y"
+    def triggerMovement(self, axis, distance, velocity=10):
+        self.isMoving = True
+        self.distanceToMove = distance
+        self.movementVelocity = velocity
+
+        if axis == "x":
+            self.orientation = ["left", "right"][int(distance > 0)]
+            
+        if axis == "y":
+            self.orientation = ["up", "down"][int(distance > 0)]
+
+    def update(self):
+        if self.isMoving:
+            if self.distanceToMove != 0:
+                if self.orientation in ["down", "right"]:
+                    self.position[[0, 1][{"right": 0, "down": 1}[self.orientation]]] += min(self.movementVelocity, self.distanceToMove)
+                    self.distanceToMove = max(0, self.distanceToMove - abs(self.movementVelocity))
+                    
+                else:
+                    self.position[[0, 1][{"left": 0, "up": 1}[self.orientation]]] += max(-self.movementVelocity, self.distanceToMove)
+                    self.distanceToMove = min(0, self.distanceToMove + abs(self.movementVelocity))
+
+                if self.distanceToMove == 0:
+                    self.isMoving = False
+                    
+                    
+    def getImage(self):
+        return self.images[self.orientation]
+            
 gameApp = Game()
 
 if __name__ == "__main__":
